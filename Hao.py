6772ae5b4f2c4c8d9fe1cd3d24ac4481cfa037a4
qpzbpn87wb-dot <qpzbpn87wb@privaterelay.appleid.com>
2026-03-18 01:15:32 +0800

    feat: upload my first python script

---
+++ Hao.py
@@ -0,0 +1,18 @@
+from sys import argv
+if len(argv) < 2:
+    print("Please provide a filename.")
+else:
+    file = open(argv[1])
+    lines = file.read()
+    lines = lines.splitlines()  # a list of strings
+    word_count = 0
+    letter_count = 0
+    for line in lines:
+        words = line.split()
+        word_count += len(words)
+        letter_count += len(line)
+
+    line_count = len(lines)
+    print(f"The line count is {line_count}")
+    print(f"The word count is {word_count}")
+    print(f"The letter count is {letter_count}")
